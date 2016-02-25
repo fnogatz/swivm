c20d42b07a971e1842853ed4de8d3c0cb082f56b
Falco Nogatz <fnogatz@gmail.com>
2016-02-25 03:28:38 +0100

    v0.2.1

--- swivm.sh
+++ swivm.sh
@@ -1407,7 +1407,7 @@ swivm() {
       swivm_remote_version "$2"
     ;;
     "--version" )
-      echo "0.2.0"
+      echo "0.2.1"
     ;;
     "unload" )
       unset -f swivm swivm_print_versions \
